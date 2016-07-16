67004741a7b38bdcc3035673e60ee40c049c469d
Kieran Doonan <kieran.doonan@cloudreach.co.uk>
2016-07-16 14:11:15 +0100

    added another call to first test

--- test/test_policy_gen.py
+++ test/test_policy_gen.py
@@ -17,14 +17,19 @@ class TestPolicyGen:
 
     def test_records_and_generates_policy(self):
         self.ec2_stubber.add_response('describe_instances', {}, {})
+        self.ec2_stubber.add_response('describe_images', {}, {})
         self.ec2_stubber.activate()
 
         policy_gen = BotoPolicyGen([self.ec2])
         policy_gen.record()
+
         self.ec2.describe_instances()
+        self.ec2.describe_images()
+
         policy = json.loads(policy_gen.generate())
 
         assert 'Statement' in policy
         assert len(policy['Statement']) == 1
-        assert len(policy['Statement'][0]['Action']) == 1
-        assert policy['Statement'][0]['Action'][0] == 'ec2:DescribeInstances'
+        assert len(policy['Statement'][0]['Action']) == 2
+        assert 'ec2:DescribeInstances' in policy['Statement'][0]['Action']
+        assert 'ec2:DescribeImages' in policy['Statement'][0]['Action']
