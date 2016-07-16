0c6fe127e9d7b65de829944087d89d9b87a895b7
Kieran Doonan <kieran.doonan@cloudreach.co.uk>
2016-07-16 14:08:28 +0100

    basic functionality now works

--- policy_gen/boto3.py
+++ policy_gen/boto3.py
@@ -1,12 +1,43 @@
+from botocore.endpoint import Endpoint
+import json
+
 class BotoPolicyGen:
-    def __init__(self):
-        pass
+    def __init__(self, clients=[]):
+        self.actions = set()
+        self.clients = clients
+
+    def add_client(self, client):
+        self.clients.append(client)
 
     def record(self):
-        pass
+        for client in self.clients:
+            client.meta.events.register_first(
+                'before-call.*.*',
+                self._get_response_handler,
+                id(self)
+            )
 
     def generate(self):
-        return '{}'
+        policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": list(self.actions),
+                    "Effect": "Allow",
+                    "Resource": "*"
+                }
+            ]
+        }
 
-    def clear(self):
-        pass
+        return json.dumps(policy)
+
+    def clear_actions(self):
+        self.actions = set()
+
+    def _get_response_handler(self, model, params, **kwargs):
+        action = '{}:{}'.format(
+            model.metadata['endpointPrefix'],
+            model.name
+        )
+
+        self.actions.add(action)
