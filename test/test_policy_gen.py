6f81597a7b8eb551e4eb774ff181d08eea34ef0a
Kieran Doonan <kieran.doonan@cloudreach.co.uk>
2016-07-16 14:16:50 +0100

    changed format of client/stub generation

--- test/test_policy_gen.py
+++ test/test_policy_gen.py
@@ -6,25 +6,25 @@ from policy_gen.boto3 import BotoPolicyGen
 
 class TestPolicyGen:
 
-    def setUp(self):
+    def gen_client_and_stub(self, endpoint):
         session = boto3.Session(region_name='eu-west-1')
-        self.ec2 = session.client('ec2')
-        self.ec2_stubber = Stubber(self.ec2)
+        client = session.client(endpoint)
+        stub = Stubber(client)
 
-    def tearDown(self):
-        self.ec2 = None
-        self.ec2_stubber = None
+        return (client, stub)
 
     def test_records_and_generates_policy(self):
-        self.ec2_stubber.add_response('describe_instances', {}, {})
-        self.ec2_stubber.add_response('describe_images', {}, {})
-        self.ec2_stubber.activate()
+        ec2, stub = self.gen_client_and_stub('ec2')
 
-        policy_gen = BotoPolicyGen([self.ec2])
+        stub.add_response('describe_instances', {}, {})
+        stub.add_response('describe_images', {}, {})
+        stub.activate()
+
+        policy_gen = BotoPolicyGen([ec2])
         policy_gen.record()
 
-        self.ec2.describe_instances()
-        self.ec2.describe_images()
+        ec2.describe_instances()
+        ec2.describe_images()
 
         policy = json.loads(policy_gen.generate())
 
@@ -33,3 +33,6 @@ class TestPolicyGen:
         assert len(policy['Statement'][0]['Action']) == 2
         assert 'ec2:DescribeInstances' in policy['Statement'][0]['Action']
         assert 'ec2:DescribeImages' in policy['Statement'][0]['Action']
+
+    def test_records_policy_from_multiple_clients(self):
+        pass
