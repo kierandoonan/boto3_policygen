40af937cb6bfe003450655c03d6b109d63ecf10b
Kieran Doonan <kieran.doonan@cloudreach.co.uk>
2016-07-18 19:41:19 +0100

    added download link to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     description='Generates policy documents from calls to boto3 / botocore',
     url='https://github.com/kierandoonan/boto3_policygen',
+    download_url='https://github.com/kierandoonan/boto3_policygen/archive/0.1.tar.gz',
     author='Kieran Doonan',
     author_email='kieran@doonan.net',
     license='MIT',
